1b125e44bdd513c3418cb27513030f69a5211d47
Yuzhong23 <xixixixifatur@gmail.com>
2024-06-13 11:52:33 +0700

    Membuat void search

--- BinarySearchTress_0157.cpp
+++ BinarySearchTress_0157.cpp
@@ -57,6 +57,21 @@ public:
             parent->rightchild = newNode; // Make the right child of the parent point to new node
         }
     }
+
+    void search(string element, Node*& parent, Node*& currentNode)
+    {
+        //this function searches the currentNode of the specifield Node as null as the current Node of itw parrent
+        currentNode = ROOT;
+        parent = nullptr;
+        while ((currentNode != nullptr) && (currentNode->info != element))
+        {
+            parent = currentNode;
+            if (element < currentNode->info)
+                currentNode = currentNode->leftchild;
+            else
+                currentNode = currentNode->rightchild;
+        }
+    }
 };
 
 int main()
